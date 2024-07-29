940c686182f25f32f7688fc93c79ae88a941bd60
Conrado julio <conradojuliosisnero@gmail.com>
2024-07-29 08:13:13 -0500

    logo de la app mas dependecias

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ["expo", "prettier"],
+  extends: ["universe/native", "prettier"],
   plugins: ["prettier"],
   rules: {
-    "prettier/prettier": "error",
+    "prettier/prettier": ["error"],
   },
 };
