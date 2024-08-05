eb3b44cf479baac4e1525469b403dec656ee25a5
Conrado julio <conradojuliosisnero@gmail.com>
2024-08-05 08:18:41 -0500

    icons links install dep file-icons refactor-about-page

---
+++ components/Icons.jsx
@@ -0,0 +1,5 @@
+import Feather from "@expo/vector-icons/Feather";
+
+export const CircleIcon = ({size}) => <Feather name="info" size={size} color="white"/>;
+
+export const HomeIcon = ({size}) => <Feather name="home" size={size} color="white" />;
